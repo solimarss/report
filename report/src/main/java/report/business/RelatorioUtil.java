a4b07466e296074fdb121bcabbb93cc615b8efb6
solimarss <solimarss@rpawsgweb04.redetre.jus.br>
2016-06-29 15:04:46 -0300

    sa

--- report/src/main/java/report/business/RelatorioUtil.java
+++ report/src/main/java/report/business/RelatorioUtil.java
@@ -13,31 +13,37 @@ import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.util.JRLoader;
 
-public class RelatorioUtil{
-
-	
+public class RelatorioUtil {
 
 	public <T> void geraPdf(List<T> dados, String reportFilePath, Map<String, Object> params, OutputStream saida) {
 
 		try {
 			params.put(JRParameter.REPORT_LOCALE, new Locale("pt", "BR"));
-			
+
 			// compila jrxml em memoria
-			//JasperReport report = JasperCompileManager.compileReport(jrxml);
+			// JasperReport report = JasperCompileManager.compileReport(jrxml);
 
 			JasperReport report = (JasperReport) JRLoader.loadObject(getClass().getClassLoader().getResource(reportFilePath));
 			JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(dados);
 			JasperPrint print = JasperFillManager.fillReport(report, params, dataSource);
-		
-			
 
-			// exporta 			
 			JasperExportManager.exportReportToPdfStream(print, saida);
-
 
 		} catch (Exception e) {
 			throw new RuntimeException("Erro ao gerar relatório", e);
 		}
 	}
+
+	/*
+	 * public enum FileType {
+	 * 
+	 * PDF("pdf"), HTML("html"), XLS("xls"), ODS("ods");
+	 * 
+	 * private String label;
+	 * 
+	 * private FileType(String label) { this.label = label; }
+	 * 
+	 * public String toString() { return label; } }
+	 */
 
 }
